513c50b09b3b240e2668a71262569733e81a7b17
Абрамович Даниил Владимирович <318201@college.local>
2021-03-16 09:34:18 +0300

    add class count

---
+++ Count.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITBIT
+{
+    class Count
+    {
+        public string name;
+        public int number;
+        public double sum;
+        public Count()
+        {
+            this.name = "Счёт";
+            this.number = 1;
+            this.sum = 0;
+        }
+        public Count(string cName, int cNumber, double cSum)
+        {
+            this.name = cName;
+            this.number = cNumber;
+            this.sum = cSum;
+        }
+    }
+}
